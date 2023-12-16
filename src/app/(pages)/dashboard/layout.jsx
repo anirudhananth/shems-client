f39b13530e3cad7febd57f20edcef3213be306ae
Anirudh Ananthasubramaniam <anirudhananth99@gmail.com>
2023-12-15 23:17:33 -0500

    UI

--- src/app/(pages)/dashboard/layout.jsx
+++ src/app/(pages)/dashboard/layout.jsx
@@ -65,7 +65,11 @@ export default function Component({ children }) {
           <Link className={`flex items-center mt-2 py-2 px-6 text-gray-500 hover:bg-gray-200 hover:rounded-md hover:text-gray-700 ${
                 pathname == "/dashboard/profile" ? 'text-gray-200 rounded-md font-semibold bg-gray-600 hover:text-gray-100 hover:bg-gray-600' : 'text-gray-500'
               }`} href="/dashboard/profile">
-            <LayoutDashboardIcon className="h-6 w-6" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+</svg>
+
+
             <span className="mx-4 font-medium">Profile</span>
           </Link>
           <Link className={`flex items-center mt-2 py-2 px-6 text-gray-500 hover:bg-gray-200 hover:rounded-md hover:text-gray-700 ${
